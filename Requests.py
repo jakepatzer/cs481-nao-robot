0aa140ee96082c97c2f70ce03611747f0bbcb0cc
mafuam <makanimafua@gmail.com>
2019-11-20 13:19:30 -0800

    Update Requests.py
    Added the functionality to lookup words via Merriam-Webster

--- Requests.py
+++ Requests.py
@@ -4,6 +4,7 @@ import re
 
 
 #param "term" is the formatted search criteria for IMDB
+
 def IMDBSearch(term):
 
     #requests.get loads the html into the variable "page"
@@ -28,28 +29,45 @@ def getDescription(link):
     myTitle = soup.find("div", class_="title_wrapper")
     for child in myTitle.find_all("div"):
         child.decompose()
+    print(myTitle.text)
     #strip the preceding spaces
-    summary =str.lstrip(myDiv.text)
+    summary = str.lstrip(myDiv.text)
     #print the summary
     print(summary)
+
+def searchDictionary(word):
+
+    page = requests.get("https://www.merriam-webster.com/dictionary/" + word)
+    soup = BeautifulSoup(page.content, "html.parser")
+    myDiv = soup.find("span", class_="dtText")
+    for child in myDiv.find_all("span"):
+        child.decompose()
+    print(myDiv.text)
 
 def main():
 
     #Take in input from the user and format it as required for IMDB
     #Example: it chapter 2 -> it+chapter+2
-    search = input("Search something: ")
-    split = search.split(" ")
-    searchTerm = ""
-    for i in range(0, len(split)):
-        if (i == len(split) - 1):
+    searchType = input("What would you like to search?: Movie or Word ")
+    str.capitalize(searchType)
+    if (searchType.lower() == 'movie'):
+        search = input("Search something: ")
+        split = search.split(" ")
+        searchTerm = ""
+        for i in range(0, len(split)):
+            if (i == len(split) - 1):
+                searchTerm += split[i]
+                break
             searchTerm += split[i]
-            break
-        searchTerm += split[i]
-        searchTerm += "+"
-    #gets the URL for the movie we're searching for
-    movieLink = IMDBSearch(searchTerm)
-    #prints the description of the movie we searched for
-    getDescription(movieLink)
+            searchTerm += "+"
+        #gets the URL for the movie we're searching for
+        movieLink = IMDBSearch(searchTerm)
+        #prints the description of the movie we searched for
+        getDescription(movieLink)
+    else:
+
+        search = input("Enter a word: ")
+        searchDictionary(search)
 
 if __name__ == "__main__":
     main()
