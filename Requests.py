ff10a524bd220094f1dbb382cac292a770b695b2
mafuam <makanimafua@gmail.com>
2019-11-20 15:28:46 -0800

    Update Requests.py
    Added searching for recipes. Give the name of a dish and a list of ingredients followed by a list of instructions is printed.

--- Requests.py
+++ Requests.py
@@ -52,11 +52,74 @@ def searchDictionary(word):
         return
     print(myDiv.text)
 
+def getRecipeURL(foodItem):
+
+    page = requests.get("https://www.allrecipes.com/search/results/?wt=" + foodItem)
+    soup = BeautifulSoup(page.content, "html.parser")
+    recipeCard = soup.find("div", class_="grid-card-image-container")
+    links = recipeCard.find_all('a', href=True)
+    try :
+        #print(links[0].get('href'))
+        return links[0].get('href')
+    except:
+        print("No recipe found with that name")
+        return ""
+
+def getRecipe(url):
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    mySection = soup.find("div", class_="recipe-shopper-wrapper")
+    try:
+        ingredients = mySection.find_all("span", class_="ingredients-item-name")
+        print("----------Ingredients------------")
+        for ingredient in ingredients:
+            nextLine = str.lstrip(ingredient.text)
+            print(nextLine)
+    except:
+        mySection = soup.find("ul", class_="checklist dropdownwrapper list-ingredients-1")
+        ingredients = mySection.find_all("span", class_="recipe-ingred_txt added")
+        mySection = soup.find("ul", class_="checklist dropdownwrapper list-ingredients-2")
+        ingredients2 = mySection.find_all("span", class_="recipe-ingred_txt added")
+        print("----------Ingredients------------")
+        for ingredient in ingredients:
+            nextLine = str.lstrip(ingredient.text)
+            print(nextLine)
+        for ingredient in ingredients2:
+            nextLine = str.lstrip(ingredient.text)
+            print(nextLine)
+
+
+
+def getDirections(url):
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    mySection = soup.find("section", class_="recipe-instructions recipe-instructions-new component container")
+    try:
+        directions = mySection.find_all("div", class_="section-body")
+        print("----------Directions------------")
+        for i in range(0, len(directions)):
+            step = i + 1
+            nextLine = str.lstrip(directions[i].text)
+            print("Step " + str(step) + ": " + nextLine)
+    except:
+        mySection = soup.find("div", class_="directions--section")
+        directions = mySection.find_all("span", class_="recipe-directions__list--item")
+        print("----------Directions------------")
+        for i in range(0, len(directions)):
+            step = i + 1
+            nextLine = str.lstrip(directions[i].text)
+            if(len(nextLine) == 0):
+               break
+            else:
+               print("Step " + str(step) + ": " + nextLine)
+
 def main():
 
     #Take in input from the user and format it as required for IMDB
     #Example: it chapter 2 -> it+chapter+2
-    searchType = input("What would you like to search?: Movie or Word ")
+    searchType = input("What would you like to search?: Movie, Word or Recipe ")
     str.capitalize(searchType)
     if (searchType.lower() == 'movie'):
         search = input("Search something: ")
@@ -73,10 +136,26 @@ def main():
         #prints the description of the movie we searched for
         if (len(movieLink) > 0):
             getDescription(movieLink)
-    else:
+    elif (searchType.lower() == 'word'):
 
         search = input("Enter a word: ")
         searchDictionary(search)
+
+    else:
+
+        search = input("Enter a dish you'd like to hear about: ")
+        split = search.split(" ")
+        searchTerm = ""
+        for i in range(0, len(split)):
+            if  (i == len(split) - 1):
+                searchTerm += split[i]
+                break
+            searchTerm += split[i]
+            searchTerm += "%20"
+
+        recipeURL = getRecipeURL(searchTerm)
+        getRecipe(recipeURL)
+        getDirections(recipeURL)
 
 if __name__ == "__main__":
     main()
